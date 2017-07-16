a96f731c4565f7724cd2fc8e347de8bcfb8d8620
Andrew Alderson <andrew@andrewalderson.com>
2017-07-15 20:24:42 -0600

    Added migrations for user name

--- src/IdentityServer/Data/Migrations/20170716015433_AddNameToUser.Designer.cs
+++ src/IdentityServer/Data/Migrations/20170716015433_AddNameToUser.Designer.cs
@@ -8,9 +8,10 @@ using OidcDemo.Services.Identity.Data;
 namespace OidcDemo.Services.Identity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170716015433_AddNameToUser")]
+    partial class AddNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
