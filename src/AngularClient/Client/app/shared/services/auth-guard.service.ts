9be305debc526a31e555a33de8dfb875903be7f7
Andrew Alderson <andrew@andrewalderson.com>
2017-07-21 02:11:37 -0600

    Removed log statements

--- src/AngularClient/Client/app/shared/services/auth-guard.service.ts
+++ src/AngularClient/Client/app/shared/services/auth-guard.service.ts
@@ -10,19 +10,16 @@ export class AuthGuardService implements CanActivate, CanLoad {
     constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        console.log("CAN ACTIVATE CALLED");
         return this.isAuthenticated();
     }
 
     canLoad(route: Route): Observable<boolean> {
-        console.log("CAN LOAD CALLED")
         return this.isAuthenticated();
     }
 
     private isAuthenticated(): Observable<boolean> {
         let isAuthenticated = this.authService.authenticationChallenge$;
         isAuthenticated.subscribe(authenticated => {
-            console.log("IS AUTHENTICATED: " + authenticated)
             if (!authenticated) {
                 this.authService.signin();
             }
