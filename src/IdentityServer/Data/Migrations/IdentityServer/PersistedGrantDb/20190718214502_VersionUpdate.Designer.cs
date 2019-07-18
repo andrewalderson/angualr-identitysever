212f73c41bfc89331b40e1c51993cedf4230cbf0
Andrew Alderson <andrew@andrewalderson.com>
2019-07-18 15:50:42 -0600

    updated identityserver project migrations for PersistedGrantDbContext

--- src/IdentityServer/Data/Migrations/IdentityServer/PersistedGrantDb/20190718214502_VersionUpdate.Designer.cs
+++ src/IdentityServer/Data/Migrations/IdentityServer/PersistedGrantDb/20190718214502_VersionUpdate.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace OidcDemo.Services.Identity.Data.Migrations.IdentityServer.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190718214502_VersionUpdate")]
+    partial class VersionUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
